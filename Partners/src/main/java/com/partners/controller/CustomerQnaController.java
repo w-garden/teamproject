da65920eb66a66b73a647bd69d479a27f8050716
w-garden <w.garden316@gmail.com>
2022-09-19 00:52:47 +0900

    MVC 모델 2 구조틀 추가

--- Partners/src/main/java/com/partners/controller/CustomerQnaController.java
+++ Partners/src/main/java/com/partners/controller/CustomerQnaController.java
@@ -1,17 +1,15 @@
 package com.partners.controller;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PartnersLoginController implements Action {
+public class CustomerQnaController implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
 		ActionForward forward = new ActionForward();
-		forward.setRedirect(true);
-		forward.setPath("/Partners/signin.jsp");
+		forward.setRedirect(false);
+		forward.setPath("/customer/qna.jsp");
 		return forward;
 	}
 
