5f8c42eb9160e6446159806386743b4a4247d151
Dandel <5140lovelyl@gmail.com>
2022-09-20 19:37:45 +0900

    관리자문의글 작성 구현완료

---
+++ DoZip/src/main/java/com/dozip/vo/QnaVO.java
@@ -0,0 +1,105 @@
+package com.dozip.vo;
+
+public class QnaVO { //14개
+	private int qna_no;
+	private String mem_id;
+	private String business_num;
+	private String qna_type;
+	private String qna_title;
+	private String qna_cont;
+	private String qna_date;
+	private String edit_date;
+	private int qna_state;
+	private int qna_ref;
+	private int qna_step;
+	private int qna_level;
+	private String reply_state;
+	private String reply_date;
+	
+	public int getQna_no() {
+		return qna_no;
+	}
+	public void setQna_no(int qna_no) {
+		this.qna_no = qna_no;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getBusiness_num() {
+		return business_num;
+	}
+	public void setBusiness_num(String business_num) {
+		this.business_num = business_num;
+	}
+	public String getQna_type() {
+		return qna_type;
+	}
+	public void setQna_type(String qna_type) {
+		this.qna_type = qna_type;
+	}
+	public String getQna_title() {
+		return qna_title;
+	}
+	public void setQna_title(String qna_title) {
+		this.qna_title = qna_title;
+	}
+	public String getQna_cont() {
+		return qna_cont;
+	}
+	public void setQna_cont(String qna_cont) {
+		this.qna_cont = qna_cont;
+	}
+	public String getQna_date() {
+		return qna_date;
+	}
+	public void setQna_date(String qna_date) {
+		this.qna_date = qna_date;
+	}
+	public String getEdit_date() {
+		return edit_date;
+	}
+	public void setEdit_date(String edit_date) {
+		this.edit_date = edit_date;
+	}
+	public int getQna_state() {
+		return qna_state;
+	}
+	public void setQna_state(int qna_state) {
+		this.qna_state = qna_state;
+	}
+	public int getQna_ref() {
+		return qna_ref;
+	}
+	public void setQna_ref(int qna_ref) {
+		this.qna_ref = qna_ref;
+	}
+	public int getQna_step() {
+		return qna_step;
+	}
+	public void setQna_step(int qna_step) {
+		this.qna_step = qna_step;
+	}
+	public int getQna_level() {
+		return qna_level;
+	}
+	public void setQna_level(int qna_level) {
+		this.qna_level = qna_level;
+	}
+	public String getReply_state() {
+		return reply_state;
+	}
+	public void setReply_state(String reply_state) {
+		this.reply_state = reply_state;
+	}
+	public String getReply_date() {
+		return reply_date;
+	}
+	public void setReply_date(String reply_date) {
+		this.reply_date = reply_date;
+	}
+	
+	
+}
--- DoZip/src/main/webapp/counsel/counsel_write.jsp
+++ DoZip/src/main/webapp/counsel/counsel_write.jsp
@@ -9,22 +9,21 @@
 		<p>고객센터 - 문의하기</p>
 	</div>
 	<div class="counsel_write_form_box">
-		<form id="counsel_write_form" action="" method="post">
+		<form id="counsel_write_form" action="counsel_write_ok.do" method="post">
 			<span>제목<br/></span>
-			<input type="text" name="cw_title"  id="cw_title"  placeholder="제목을 입력해주세요"/><br/>
+			<input type="text" name="qna_title"  id="qna_title"  placeholder="제목을 입력해주세요"/><br/>
 			<span>문의유형<br/></span>
-			<!-- <input type="text" name="cw_check"  id="cw_check"  placeholder="제목을 입력해주세요"/><br/> <select name="choice1" class="choice_group"> -->
-			<select name="cw_check" id="cw_check">
+			<select name="qna_type" id="qna_type">
 				<option selected>문의유형</option>
-				<option value="1">1번 문의유형</option>
-				<option value="2">2번 문의유형</option>
-				<option value="3">3번 문의유형</option>
-				<option value="4">4번 문의유형</option>
-				<option value="5">5번 문의유형</option>
+				<option value="1번 문의유형">1번 문의유형</option>
+				<option value="2번 문의유형">2번 문의유형</option>
+				<option value="3번 문의유형">3번 문의유형</option>
+				<option value="4번 문의유형">4번 문의유형</option>
+				<option value="5번 문의유형">5번 문의유형</option>
 			</select><br />
 			<span>문의내용<br /></span>
-			<textarea name="cw_cont" id="cw_cont" placeholder="문의내용을 상세히 적어주시면 상담에 도움이 됩니다."></textarea><br/>
-    		<div class="cw_btn_wrap"><button type="button" id="cw_btn">작성완료</button></div>
+			<textarea name="qna_cont" id="qna_cont" placeholder="문의내용을 상세히 적어주시면 상담에 도움이 됩니다."></textarea><br/>
+    		<div class="cw_btn_wrap"><button type="submit" id="qna_btn">작성완료</button></div>
 		</form>	
 	</div>
 
